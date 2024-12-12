f8bd2b3ffd6d0c666a90f28b842a4864d0767fd8
madhusudan9332 <madhusudangautam003@gmail.com>
2024-12-12 23:49:48 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
       ? "https://twitter-clone-login.onrender.com"
-      : "http://localhost:10000",
+      : "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
